0d3304c72b2aa4baafb2a1c58f599fc83dd62348
Faldiansyah <faldiansyahk@gmail.com>
2022-10-05 00:52:31 +0800

    read data

--- app/src/main/java/com/example/api_project_group/MainActivity.kt
+++ app/src/main/java/com/example/api_project_group/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // https://6254434289f28cf72b5aed04.mockapi.io/film
+        // https://6254434289f28cf72b5aed04.mockapi.io/user
+
+        
+
     }
 }
