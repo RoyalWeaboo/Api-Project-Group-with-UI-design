01a9b6ec49cf1d2e84811254f9ac97fa5ac4f041
RoyalWeaboo <malikazizali86@gmail.com>
2022-10-05 10:48:31 +0700

    adding delete function

--- app/src/main/java/com/example/api_project_group/activity/MainActivity.kt
+++ app/src/main/java/com/example/api_project_group/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.api_project_group.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -20,20 +21,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.mainToolbar.setTitle("List Film")
+
         setVmToAdapter()
     }
 
     private fun setVmToAdapter() {
         val viewModel =ViewModelProvider(this).get(ViewModelFilm::class.java)
-        viewModel.callApiFilm()
         viewModel.getliveDataFilm().observe(this, Observer {
             filmAdapter = AdapterFilm(it)
-            if (it != null){
+            if (it != null) {
                 binding.rvFilm.layoutManager = LinearLayoutManager(
                     this, LinearLayoutManager.VERTICAL, false
                 )
-                binding.rvFilm.adapter = AdapterFilm(it)
+                filmAdapter = AdapterFilm(it)
+                binding.rvFilm.adapter = filmAdapter
 
+                filmAdapter.onDelete={
+                    viewModel.callDeleteFilm(it)
+                    viewModel.getdeleteFilm().observe(this, Observer {
+                        if (it == null){
+                            Toast.makeText(this, "Deleted Film with id : $it", Toast.LENGTH_LONG).show()
+                        }
+                    })
+                }
+                filmAdapter.notifyDataSetChanged()
+            }else{
+                Toast.makeText(this, "There is no data to show", Toast.LENGTH_LONG).show()
             }
 
             filmAdapter.onDetail ={
@@ -44,6 +58,7 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         })
+        viewModel.callApiFilm()
     }
 
 }
--- app/src/main/java/com/example/api_project_group/viewmodel/ViewModelFilm.kt
+++ app/src/main/java/com/example/api_project_group/viewmodel/ViewModelFilm.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.api_project_group.model.RestponseDataFilmItem
 import com.example.api_project_group.network.RetrofitFilm
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -11,36 +13,69 @@ import retrofit2.Response
 class ViewModelFilm : ViewModel() {
 
     lateinit var liveDataFilm : MutableLiveData<List<RestponseDataFilmItem>>
+    var deleteFilm : MutableLiveData<Int>
 
     init {
         liveDataFilm = MutableLiveData()
 //        addLiveDataCar = MutableLiveData()
 //        updLDcar = MutableLiveData()
-//        delCar = MutableLiveData()
+        deleteFilm = MutableLiveData()
+        callApiFilm()
     }
     fun getliveDataFilm() :MutableLiveData<List<RestponseDataFilmItem>>{
         return  liveDataFilm
     }
 
+    fun getdeleteFilm(): MutableLiveData<Int> {
+        return deleteFilm
+    }
+
     fun callApiFilm(){
-        RetrofitFilm.instance.getAllFilm()
-            .enqueue(object : Callback<List<RestponseDataFilmItem>>{
-                override fun onResponse(
-                    call: Call<List<RestponseDataFilmItem>>,
-                    response: Response<List<RestponseDataFilmItem>>
-                ) {
-                    if (response.isSuccessful){
-                        liveDataFilm.postValue(response.body())
-                    }else{
+        GlobalScope.async {
+            RetrofitFilm.instance.getAllFilm()
+                .enqueue(object : Callback<List<RestponseDataFilmItem>>{
+                    override fun onResponse(
+                        call: Call<List<RestponseDataFilmItem>>,
+                        response: Response<List<RestponseDataFilmItem>>
+                    ) {
+                        if (response.isSuccessful){
+                            liveDataFilm.postValue(response.body())
+                        }else{
+                            liveDataFilm.postValue(null)
+                        }
+                    }
+
+                    override fun onFailure(call: Call<List<RestponseDataFilmItem>>, t: Throwable) {
                         liveDataFilm.postValue(null)
                     }
-                }
 
-                override fun onFailure(call: Call<List<RestponseDataFilmItem>>, t: Throwable) {
-                    liveDataFilm.postValue(null)
-                }
-
-            })
-
+                })
+        }
     }
+
+
+    fun callDeleteFilm(id: Int) {
+        GlobalScope.async {
+            RetrofitFilm.instance.deleteFilm(id)
+                .enqueue(object : Callback<Int> {
+                    override fun onResponse(
+                        call: Call<Int>,
+                        response: Response<Int>
+                    ) {
+                        if (response.isSuccessful) {
+                            deleteFilm.postValue(response.body())
+                        } else {
+                            deleteFilm.postValue(null)
+                        }
+                        callApiFilm()
+                    }
+
+                    override fun onFailure(call: Call<Int>, t: Throwable) {
+                        deleteFilm.postValue(null)
+                        callApiFilm()
+                    }
+                })
+        }
+    }
+
 }
